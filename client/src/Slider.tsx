a7b5caa0bc43446d7307b32f828c6e91aa62e1a7
motigra <mrmag88@gmail.com>
2022-10-30 19:00:24 +0200

    react app bootstrap

---
+++ client/src/Slider.tsx
@@ -0,0 +1,58 @@
+import { throws } from 'assert';
+import React from 'react';
+
+type SliderState = {
+    value: number,
+    muted: boolean
+};
+
+type SliderProps = {
+    title?: string,
+    value?: number,
+    muted?: boolean,
+    onValueChange?: (value: number, muted: boolean) => void
+};
+
+class Slider extends React.Component<SliderProps, SliderState> {
+
+    constructor(props: SliderProps) {
+        super(props);
+        this.state = {
+            value: props.value || 0,
+            muted: props.muted || false
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleMute = this.handleMute.bind(this);
+        this.notifyStateChange = this.notifyStateChange.bind(this);
+    }
+
+    handleChange(event: any) {
+        const newState = { value: event.target.value, muted: this.state.muted };
+        this.setState(newState);
+        this.notifyStateChange(newState);
+    }
+
+    handleMute(event: any) {
+        const newState = { value: this.state.value, muted: !this.state.muted };
+        this.setState(newState);
+        this.notifyStateChange(newState);
+    }
+
+    notifyStateChange(newState: SliderState) {
+        if (this.props.onValueChange)
+            this.props.onValueChange(newState.value, newState.muted);
+    }
+  
+    render() {
+        return (
+            <div>
+                <span>{ this.props.title }</span>
+                <span>{ this.state.value }</span>
+                <input type="range" min="0" max="100" value={this.state.value} onInput={this.handleChange} />
+                <button onClick={this.handleMute}>{this.state.muted ? 'unmute' : 'mute' }</button>
+            </div>
+        );
+    }
+}
+
+export default Slider;
